6f8cddb3294ed08eaf2a728c557a759d4d7403d0
Alessandra Grazielle <61811422+alessandragrazielle@users.noreply.github.com>
2023-08-19 18:45:14 -0300

    Add files via upload

---
+++ exercicio02/questao7.ts
@@ -0,0 +1,9 @@
+let myName: string = 'Ely';
+let paymentTime: number = 120.56;
+let language: string = 'TypeScript';
+
+console.log(
+`${myName}
+My payment time is ${paymentTime}
+and
+my preffered language is ${language}`);
