7f1ff197af2150a2f5169a6c7c58b36c5a719ddf
Alessandra Grazielle <61811422+alessandragrazielle@users.noreply.github.com>
2023-08-20 12:27:35 -0300

    Add files via upload

---
+++ exercicio02/questao5.ts
@@ -0,0 +1,12 @@
+// um objeto que recebe propriedades do tipo any
+const personalData: any = {
+    myName: 'Alessandra Grazielle',
+    cpf: '000.000.000-00',
+    myAge: 20,
+    married: false,
+}
+
+// Acessando as propriedades do objeto usando 'any'
+console.log(personalData.myName);  
+console.log(personalData.myAge);
+console.log(personalData.married);
