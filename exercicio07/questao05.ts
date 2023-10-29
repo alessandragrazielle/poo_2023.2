29ca9624ac670378d761a1b2c9eda1f10ec5bab0
Alessandra Grazielle <61811422+alessandragrazielle@users.noreply.github.com>
2023-10-29 12:55:50 -0300

    Update questao05.ts

--- exercicio07/questao05.ts
+++ exercicio07/questao05.ts
@@ -130,21 +130,3 @@ class Estoque{
     }
     
 }
-
-
-let p1: ProdutoPerecivel = new ProdutoPerecivel(1, 'a', 5, 6, new Date("2020-02-10"));
-console.log(p1.estaValido());
-
-let e: Estoque = new Estoque;
-e.inserir(p1);
-e.inserir(new Produto(2, 'arroz', 20, 5));
-e.inserir(new Produto(3, 'feijao', 10, 6));
-e.inserir(new ProdutoPerecivel(4, 'tomate', 10, 5, new Date('2020-11-10')));
-e.inserir(new Produto(5, 'macarrao', 20, 5))
-e.excluir(3)
-e.repor(4, 'tomate', 2)
-e.repor(2, 'arroz', 2)
-console.log(e.listarProdutosVencidos());
-
-
-
