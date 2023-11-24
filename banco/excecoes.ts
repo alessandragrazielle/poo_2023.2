4f008a5c9b939eca915d6877f9fe15b3f9dc8069
Alessandra Grazielle <61811422+alessandragrazielle@users.noreply.github.com>
2023-11-24 20:21:33 -0300

    Add files via upload

---
+++ banco/excecoes.ts
@@ -0,0 +1,37 @@
+class AplicacaoError extends Error{
+    constructor(message:string){
+        super(message);
+    }
+}
+
+class ContaInexistenteError extends AplicacaoError{
+    constructor(message:string){
+        super(message);
+    }
+}
+
+class ContaJaCadastradaError extends AplicacaoError{
+    constructor(message:string){
+        super(message);
+    }
+}
+
+class SaldoInsuficienteError extends AplicacaoError{
+    constructor(message:string){
+        super(message);
+    }
+}
+
+class ValorInvalidoError extends AplicacaoError{
+    constructor(message:string){
+        super(message);
+    }
+}
+
+class PoupancaInvalidaError extends Error{
+    constructor(message:string){
+        super(message);
+    }
+}
+
+export {AplicacaoError, ContaInexistenteError, SaldoInsuficienteError, ValorInvalidoError, PoupancaInvalidaError, ContaJaCadastradaError}
